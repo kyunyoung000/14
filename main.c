cb99f0955326e355876d819426b0f9b889c482c5
kyunyoung000 <kyunyoung@sookmyung.ac.kr>
2025-12-07 23:01:16 +0900

    14-2 수정

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 struct Book{
        int number;
        char title[10];
@@ -8,26 +10,21 @@ struct Book{
 
 int main(int argc, char *argv[])
 {
-    int i;
-    struct Book **bookshelf;
+    struct Book *p;
     
-    bookshelf = (struct Book**)malloc(3*sizeof(struct Book*));    
-    for(i=0; i<3; i++)
-        bookshelf[i] = (struct Book*)malloc(10*sizeof(struct Book));
-        
-    bookshelf[1][3].number = 5;
-    strcpy(bookshelf[1][3].title, "C++ Programming");
+    p = (struct Book*)malloc(2*sizeof(struct Book));
     
-    (bookshelf[2] + 4)->number = 3;
-    strcpy((bookshelf[2] + 4)->title, "Communications Theory");
+    if (p == NULL){
+          printf("¢¬¨­¢¬©£¢¬¢ç CO¢¥c ¢¯A¡¤u\n");
+          return;}
+          
+    p->number = 1;
+    strcpy(p->title, "C Programming");
     
-    printf("book (1,3) : %i, %s\n", (bookshelf[1]+3)-> number, (bookshelf[1]+3) -> title);
-    printf("book (2,4) : %i, %s\n", bookshelf[2][4].number, bookshelf[2][4].title);
+    (p+1)->number = 2;
+    strcpy((p+1)->title, "ELectronics");
     
-    for(i=0; i<3; i++)
-        free(bookshelf[i]);
-    free(bookshelf);
-        
+    free(p);
     
   
   system("PAUSE");	
